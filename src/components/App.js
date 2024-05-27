6e350c04868a7729adb008cbb890fec69cdd2e05
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-05-27 02:05:41 -0400

    State Mini Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleDeleteTask(taskText) {
+    const updatedTasks = tasks.filter(task => task.text !== taskText);
+    setTasks(updatedTasks);
+  }
+
+  function handleCategorySelect(category) {
+    setSelectedCategory(category);
+  }
+
+  function onTaskFormSubmit(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  const filteredTasks = selectedCategory === "All" ? tasks : tasks.filter(task => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+        categories={CATEGORIES} 
+        selectedCategory={selectedCategory} 
+        onSelectCategory={handleCategorySelect} 
+      />
+      <NewTaskForm 
+        categories={CATEGORIES.filter(category => category !== "All")} 
+        onTaskFormSubmit={onTaskFormSubmit} 
+      />
+      <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onDeleteTask }) {
+  
+  const taskItems = tasks.map((task) => (
+    <Task
+      text={task.text}
+      category={task.category}
+      key={task.text}
+      onDeleteTask={onDeleteTask}
+      />
+    ));
+  
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskItems}
     </div>
   );
 }
